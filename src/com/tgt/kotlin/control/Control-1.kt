19222c72c4c1b228d636dcaab0a2d39820d311e5
VineetJavaWorld <vineetsingh12144@gmail.com>
2020-07-12 12:04:49 +0530

    control flow statement

--- src/com/tgt/kotlin/control/Control-1.kt
+++ src/com/tgt/kotlin/control/Control-1.kt
@@ -1,2 +1,45 @@
 package com.tgt.kotlin.control
 
+fun main() {
+
+    var a = 1;
+    var b = 2;
+
+    //Traditional Usage
+    println("-------Traditional Usage---------")
+    var max = a
+    if (a < b)
+        max = b
+    println(max)
+
+    println("-----Expression Way-------")
+    max = if (a > b) a else b
+    println(max)
+
+    println("---------When------------")
+    when (a) {
+        1 -> println("a==1")
+        2 -> println("a==2")
+        else -> {
+            println("a is neither 1 nor 2")
+        }
+    }
+
+    when (a) {
+        0, 1 -> println("a == 0 or a ==1")
+        else -> println("otherwise")
+    }
+
+    when (a) {
+        in 1..10 -> println("a is in the range")
+        !in 10..20 -> println("a is outside the range")
+        else -> println("none of the above")
+    }
+
+    println(hasPrefix(123))
+    println(hasPrefix("prefix123"))
+    }
+    fun hasPrefix(x: Any) = when(x) {
+        is String -> x.startsWith("prefix")
+        else -> false
+    }
